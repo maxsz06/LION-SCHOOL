300500df64c4d220153bc9b32fc3de118e8ff92d
maxsz06 <maxwilliansantana04@gmail.com>
2026-07-27 09:10:52 -0300

    Cards - Criado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,39 @@
 
 import { getUsuarios } from "./usuarios.js"
 
-    const usuarios = await getUsuarios()
+async function carregarAlunos() {
+    const alunos = await getUsuarios()
+    renderizarAlunos(alunos)
+}
 
-    console.log(usuarios)
+function calcularMedia(desempenho){
+    const soma = desempenho.reduce((acc, item) => acc + item.valor, 0)
+    return (soma / desempenho.length).toFixed(1)
+}
+
+function criarCardAluno(aluno){
+    const card = document.createElement('div')
+    card.classList.add('card-aluno', aluno.status)
+
+    const media = calcularMedia(aluno.desempenho)
+
+    card.innerHTML = `
+        <img src="${aluno.foto}" alt="${aluno.nome}">
+        <h3>${aluno.nome}</h3>
+        <p class="media">Média: ${media}</p>
+    `
+
+    return card
+}
+
+function renderizarAlunos(alunos){
+    const conteiner = document.getElementById('lista-alunos')
+    conteiner.innerHTML = ''
+
+    alunos.forEach(aluno => {
+        const card = criarCardAluno(aluno)
+        conteiner.appendChild(card)
+    });
+}
+
+carregarAlunos()
